6c9fe8130b170cc88ba1ff932b5dc3357c817956
Thiago Coelho <thiago.coelho158@gmail.com>
2024-12-24 00:20:53 -0300

    feat: habilitar uso de csrf em rotas da api

--- src/proagua_api/api/__init__.py
+++ src/proagua_api/api/__init__.py
@@ -12,17 +12,8 @@ from . import (
     parametros_referencia,
     solicitacoes
 )
-# import orjson
-# from ninja.renderers import BaseRenderer
 
-
-# class ORJSONRenderer(BaseRenderer):
-#     media_type = "application/json"
-
-#     def render(self, request, data, *, response_status):
-#         return orjson.dumps(data)
-
-api = NinjaAPI(auth=auth.JWTBearer(), csrf=False)
+api = NinjaAPI(auth=auth.JWTBearer(), csrf=True)
 
 # Public routes
 @api.get("/csrf", auth=None)
