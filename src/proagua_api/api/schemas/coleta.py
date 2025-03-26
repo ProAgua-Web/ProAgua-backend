23d20350ca442c5d902a552dfc69136b70827c5f
Thiago Coelho <thiago.coelho158@gmail.com>
2025-03-26 18:50:23 -0300

    feat: renomear campos de filter de coletas

--- src/proagua_api/api/schemas/coleta.py
+++ src/proagua_api/api/schemas/coleta.py
@@ -47,82 +47,82 @@ class ColetaOut(Schema):
 
 
 class FilterColeta(FilterSchema):
-    responsavel__username__contains: str = Field(
+    responsavel__username__contains: Optional[str] = Field(
         default=None,
-        q=["responsavel__username__contains"]
+        alias="responsavel"
     )
     
-    data__gte: date = Field(
+    data__gte: Optional[date] = Field(
         default=None,
         alias="data_minima"
     )
     
-    data__lte: date = Field(
+    data__lte: Optional[date] = Field(
         default=None,
         alias="data_maxima"
     )
     
-    sequencia_id: int = Field(
+    sequencia__id: Optional[int] = Field(
         default=None,
-        alias="sequencia__id"
+        alias="sequencia_id"
     )
     
-    temperatura__gte: float = Field(
+    temperatura__gte: Optional[float] = Field(
         default=None,
         alias="temperatura_minima"
     )
     
-    temperatura__lte: float = Field(
+    temperatura__lte: Optional[float] = Field(
         default=None,
         alias="temperatura_maxima"
     )
     
-    cloro_residual_livre__gte: float = Field(
+    cloro_residual_livre__gte: Optional[float] = Field(
         default=None,
         alias="cloro_residual_livre_minimo"
     )
     
-    cloro_residual_livre__lte: float = Field(
+    cloro_residual_livre__lte: Optional[float] = Field(
         default=None,
         alias="cloro_residual_livre_maximo"
     )
     
-    turbidez__gte: float = Field(
+    turbidez__gte: Optional[float] = Field(
         default=None,
         alias="turbidez_minima"
     )
     
-    turbidez__lte: float = Field(
+    turbidez__lte: Optional[float] = Field(
         default=None,
         alias="turbidez_maxima"
     )
     
-    coliformes_totais: bool = Field(
+    coliformes_totais: Optional[bool] = Field(
         default=None,
         alias="coliformes_totais"
     )
     
-    escherichia: bool = Field(
+    escherichia: Optional[bool] = Field(
         default=None,
         alias="escherichia"
     )
     
-    cor__gte: float = Field(
+    cor__gte: Optional[float] = Field(
         default=None,
         alias="cor_minima"
     )
     
-    cor__lte: float = Field(
+    cor__lte: Optional[float] = Field(
         default=None,
         alias="cor_maxima"
     )
     
-    ordem: str = Field(
+    ordem: Optional[str] = Field(
         default=None,
         alias="ordem"
     )
 
-    ponto__edificacao__codigo__exact: str = Field(
+    ponto__edificacao__codigo__exact: Optional[str] = Field(
         default=None,
         alias="codigo_edificacao"
     )
